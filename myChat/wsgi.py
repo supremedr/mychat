9fa4a8bf60a4efdf7947b784a8a984fdd7a84955
Ahmed Zaki <supremedr22@yahoo.com>
2023-01-16 20:41:21 +0200

    deploy to railway

--- myChat/wsgi.py
+++ myChat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myChat.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myChat.settings.railway')
 
 application = get_wsgi_application()
